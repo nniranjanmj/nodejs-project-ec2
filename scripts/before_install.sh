aad6dc5aa7e1f8f8160c75351bff05250cf4e83d
nniranjanmj <131424694+nniranjanmj@users.noreply.github.com>
2023-09-12 21:35:53 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 
 #_Download_Node_&NPM
-yum -y install nodejs npm
+apt -y install nodejs npm
 
 #_Download_PM2
 npm install pm2@latest -g
