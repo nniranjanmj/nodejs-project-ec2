7c027407be404172db4aa11cd546a924a6618518
nniranjanmj <131424694+nniranjanmj@users.noreply.github.com>
2023-09-14 10:07:43 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-#_Change_Working_Directory
-cd /home/ubuntu/server
-
-#_Update_&_Set_Node_Version
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash
-
-#_Download_Node_&NPM
-sudo apt-get install nodejs npm
-
-#_Download_PM2
-sudo npm install -g pm2
+rm -rf /usr/share/nginx/html/*
